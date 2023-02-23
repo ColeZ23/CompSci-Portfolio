5811972eeb523a237ccddd63aaf077ee0fc0a443
ColeZ23 <Cole.Z23@k12.xrds.org>
2023-02-23 10:09:39 -0800

    Initial Commit

---
+++ CompSci Portfolio/DadJokes/DadJokes/DadJokesApp.swift
@@ -0,0 +1,18 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by Cole Zytko on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
---
+++ CompSci Portfolio/DadJokes/DadJokes/Joke.swift
@@ -0,0 +1,14 @@
+//
+//  Joke.swift
+//  DadJokes
+//
+//  Created by Cole Zytko on 12/15/22.
+//
+
+import Foundation
+
+
+struct Joke: Codable{
+    var id:String
+    var joke:String
+}
